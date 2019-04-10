792870e5f821b187ccae5670d409fdb45bcfa2a5
Tyson Norris <tnorris@adobe.com>
2019-04-09 20:42:21 -0700

    use delete script instead of serverless remove

--- delete.sh
+++ delete.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #since serverless does not delete dynamic named packages...
-curl -X DELETE -u "$OW_AUTH" "$OW_APIHOST/api/v1/namespaces/_/actions/cosmos-deploytest$CIRCLE_BUILD_NUM/hello"
-curl -X DELETE -u "$OW_AUTH" "$OW_APIHOST/api/v1/namespaces/_/actions/cosmos-deploytest$CIRCLE_BUILD_NUM/hello2"
-curl -X DELETE -u "$OW_AUTH" "$OW_APIHOST/api/v1/namespaces/_/actions/cosmos-deploytest2$CIRCLE_BUILD_NUM/hello"
-curl -X DELETE -u "$OW_AUTH" "$OW_APIHOST/api/v1/namespaces/_/actions/cosmos-deploytest2$CIRCLE_BUILD_NUM/hello2"
-curl -X DELETE -u "$OW_AUTH" "$OW_APIHOST/api/v1/namespaces/_/packages/cosmos-deploytest$CIRCLE_BUILD_NUM"
-curl -X DELETE -u "$OW_AUTH" "$OW_APIHOST/api/v1/namespaces/_/packages/cosmos-deploytest2$CIRCLE_BUILD_NUM"
+curl -X DELETE -u "$OW_AUTH" "$OW_APIHOST/api/v1/namespaces/_/actions/cosmos-deploytest-$CIRCLE_BUILD_NUM/hello"
+curl -X DELETE -u "$OW_AUTH" "$OW_APIHOST/api/v1/namespaces/_/actions/cosmos-deploytest-$CIRCLE_BUILD_NUM/hello2"
+curl -X DELETE -u "$OW_AUTH" "$OW_APIHOST/api/v1/namespaces/_/actions/cosmos-deploytest2-$CIRCLE_BUILD_NUM/hello"
+curl -X DELETE -u "$OW_AUTH" "$OW_APIHOST/api/v1/namespaces/_/actions/cosmos-deploytest2-$CIRCLE_BUILD_NUM/hello2"
+curl -X DELETE -u "$OW_AUTH" "$OW_APIHOST/api/v1/namespaces/_/packages/cosmos-deploytest-$CIRCLE_BUILD_NUM"
+curl -X DELETE -u "$OW_AUTH" "$OW_APIHOST/api/v1/namespaces/_/packages/cosmos-deploytest2-$CIRCLE_BUILD_NUM"
