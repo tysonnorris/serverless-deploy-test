be51747958710ff199601ae8f4a44d921749207b
Tyson Norris <tnorris@adobe.com>
2019-04-09 21:03:38 -0700

    debugging delete

--- delete.sh
+++ delete.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
 #since serverless does not delete dynamic named packages...
+echo "deleting cosmos-deploytest-$CIRCLE_BUILD_NUM/hello"
 curl -X DELETE -u "$OW_AUTH" "$OW_APIHOST/api/v1/namespaces/_/actions/cosmos-deploytest-$CIRCLE_BUILD_NUM/hello"
+
+echo "deleting cosmos-deploytest-$CIRCLE_BUILD_NUM/hello2"
 curl -X DELETE -u "$OW_AUTH" "$OW_APIHOST/api/v1/namespaces/_/actions/cosmos-deploytest-$CIRCLE_BUILD_NUM/hello2"
+
+echo "deleting cosmos-deploytest2-$CIRCLE_BUILD_NUM/hello"
 curl -X DELETE -u "$OW_AUTH" "$OW_APIHOST/api/v1/namespaces/_/actions/cosmos-deploytest2-$CIRCLE_BUILD_NUM/hello"
+
+echo "deleting cosmos-deploytest2-$CIRCLE_BUILD_NUM/hello2"
 curl -X DELETE -u "$OW_AUTH" "$OW_APIHOST/api/v1/namespaces/_/actions/cosmos-deploytest2-$CIRCLE_BUILD_NUM/hello2"
+
+echo "deleting cosmos-deploytest-$CIRCLE_BUILD_NUM"
 curl -X DELETE -u "$OW_AUTH" "$OW_APIHOST/api/v1/namespaces/_/packages/cosmos-deploytest-$CIRCLE_BUILD_NUM"
+
+echo "deleting cosmos-deploytest2-$CIRCLE_BUILD_NUM"
 curl -X DELETE -u "$OW_AUTH" "$OW_APIHOST/api/v1/namespaces/_/packages/cosmos-deploytest2-$CIRCLE_BUILD_NUM"
