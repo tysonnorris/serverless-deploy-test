4e36f1eb9a10e3e89f084ca4a667c4a91774d609
Tyson Norris <tnorris@adobe.com>
2019-04-09 21:23:12 -0700

    debugging delete

--- delete.sh
+++ delete.sh
@@ -5,16 +5,16 @@ echo "deleting cosmos-deploytest-$CIRCLE_BUILD_NUM/hello"
 curl -X DELETE -u "$OW_AUTH" "$OW_APIHOST/api/v1/namespaces/_/actions/cosmos-deploytest-$CIRCLE_BUILD_NUM/hello"
 
 echo "deleting cosmos-deploytest-$CIRCLE_BUILD_NUM/hello2"
-curl -X DELETE -u "$OW_AUTH" "$OW_APIHOST/api/v1/namespaces/_/actions/cosmos-deploytest-$CIRCLE_BUILD_NUM/hello2"
+curl --write-out '%{http_code}\n' --silent --output /dev/null -X DELETE -u "$OW_AUTH" "$OW_APIHOST/api/v1/namespaces/_/actions/cosmos-deploytest-$CIRCLE_BUILD_NUM/hello2"
 
 echo "deleting cosmos-deploytest2-$CIRCLE_BUILD_NUM/hello"
-curl -X DELETE -u "$OW_AUTH" "$OW_APIHOST/api/v1/namespaces/_/actions/cosmos-deploytest2-$CIRCLE_BUILD_NUM/hello"
+curl --write-out '%{http_code}\n' --silent --output /dev/null -X DELETE -u "$OW_AUTH" "$OW_APIHOST/api/v1/namespaces/_/actions/cosmos-deploytest2-$CIRCLE_BUILD_NUM/hello"
 
 echo "deleting cosmos-deploytest2-$CIRCLE_BUILD_NUM/hello2"
-curl -X DELETE -u "$OW_AUTH" "$OW_APIHOST/api/v1/namespaces/_/actions/cosmos-deploytest2-$CIRCLE_BUILD_NUM/hello2"
+curl --write-out '%{http_code}\n' --silent --output /dev/null -X DELETE -u "$OW_AUTH" "$OW_APIHOST/api/v1/namespaces/_/actions/cosmos-deploytest2-$CIRCLE_BUILD_NUM/hello2"
 
 echo "deleting cosmos-deploytest-$CIRCLE_BUILD_NUM"
-curl -X DELETE -u "$OW_AUTH" "$OW_APIHOST/api/v1/namespaces/_/packages/cosmos-deploytest-$CIRCLE_BUILD_NUM"
+curl --write-out '%{http_code}\n' --silent --output /dev/null -X DELETE -u "$OW_AUTH" "$OW_APIHOST/api/v1/namespaces/_/packages/cosmos-deploytest-$CIRCLE_BUILD_NUM"
 
 echo "deleting cosmos-deploytest2-$CIRCLE_BUILD_NUM"
-curl -X DELETE -u "$OW_AUTH" "$OW_APIHOST/api/v1/namespaces/_/packages/cosmos-deploytest2-$CIRCLE_BUILD_NUM"
+curl --write-out '%{http_code}\n' --silent --output /dev/null -X DELETE -u "$OW_AUTH" "$OW_APIHOST/api/v1/namespaces/_/packages/cosmos-deploytest2-$CIRCLE_BUILD_NUM"
